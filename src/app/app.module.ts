4fd661a1ef18da0a560bcd05b4c6c2e6661472c8
usersaras <saras.sthapitshrestha@gmail.com>
2024-01-16 20:08:54 +0545

    Setup: Setup Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
